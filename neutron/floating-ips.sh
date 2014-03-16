e1613215af75b9930589818e1f592f6f95621a4c
Sudarshan Acharya <info@sacharya.com>
2014-03-16 17:12:18 -0500

    Create floating ip for an instance

--- neutron/floating-ips.sh
+++ neutron/floating-ips.sh
@@ -12,8 +12,12 @@ neutron port-list
 
 neutron port-list  -- --device_id=$intance_id
 
+neutron floatingip-create  --port-id $instance_port_id floating-net
+
 neutron floatingip-list
 
+# Creating another floating-ip and associate it to an instance
+neutron floatingip-create floating-net
 neutron floatingip-associate $floatingip_id $instance_port_id
 
 ping $instance_floating_ip
