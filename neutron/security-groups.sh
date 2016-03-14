eea89441120d01f223e56a202d6b421c1e147485
Sudarshan Acharya <info@sacharya.com>
2016-03-13 21:58:38 -0600

    Security groups

--- neutron/security-groups.sh
+++ neutron/security-groups.sh
@@ -1,3 +1,4 @@
+# http://blog.aaronorosen.com/building-a-multi-tier-application-with-openstack/
 
 neutron security-group-rule-create --protocol icmp --direction ingress --remote-ip-prefix 0.0.0.0/0 default
 neutron security-group-rule-create --protocol tcp --port-range-min 22 --port-range-max 22 --direction ingress --remote-ip-prefix 0.0.0.0/0 default
